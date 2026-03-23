f4d847d7846642d0a31a86d82ce0139758a918ed
adityadaharwal963 <adityaprowork@gmail.com>
2026-03-23 17:01:49 +0530

    init

---
+++ src/main/java/com/rcoem/day1demo/Day1DemoApplication.java
@@ -0,0 +1,13 @@
+package com.rcoem.day1demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day1DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Day1DemoApplication.class, args);
+    }
+
+}
